68fcacba0624464d642c2471ac6360a2382c4a9b
dawani519 <danielamos519@gmail.com>
2022-10-19 01:06:59 +0000

    updated

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - primary function
+ *
+ * Description: fibonnaci number first 98
+ * Return: nothin
+ */
+int main(void)
+{
+	long int a, b, c, i;
+
+	a = 1;
+	b = 2;
+
+	printf("%i, %i, ", a, b);
+	for (i = 0; i <= 98; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		printf("%lu", c);
+
+		if (c < 5035488507601418376)
+			printf(", ");
+	}
+}
