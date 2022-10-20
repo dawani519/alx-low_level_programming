c0572ba72589ecc0a1d9f1166e60fc4bfe31dc73
dawani519 <danielamos519@gmail.com>
2022-10-20 11:21:10 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0-9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 9)
+	{
+		_putchar(i + 0);
+		i++;
+	}
+	_putchar('\n');
+}
