e36fe604d7a901ecbd27a42bdc0f2a10a1d09eba
dawani519 <danielamos519@gmail.com>
2022-11-07 20:20:04 +0000

    updated

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - entry point
+ * @argc: string
+ * @argv: array
+ * Return 0
+ */
+int main(int argc, char *argv[])
+{
+	int val;
+
+	for (val = 0; val < argc; val++)
+		printf("%s\n", argv[val]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - point
+ * @argc: string
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
